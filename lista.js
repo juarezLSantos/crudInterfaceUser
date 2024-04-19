066e7395dcf1b6349b4a15aa9dd3f22bea86ea2b
Juarez <juralima@gmail.com>
2024-04-19 16:56:59 -0300

    Atividade CrudInterfaceUser

---
+++ lista.js
@@ -0,0 +1,24 @@
+//LÓGICA DE ATUALIZAR USUÁRIO
+
+//Clicar no botão
+//Abrir o modal
+//Setar os campos com os valores do usuário do local Storage
+//Trocar o botão salvar para atualizar
+//Salvar as informações no localStorage
+//Fechar o modal
+//Recarregar a página
+
+  
+//LÓGICA DE CADASTRO
+
+//Inserir informações
+//clicar no botão salvar
+//Salvar os dados no localStorage
+//Fecha o modal
+//Recarrego a página
+
+//FUNÇÃO DE EXIBIR OS USUÁRIOS
+//Pegar os itens no localStorage(getItem)
+//Colocar os dados dentro de um array
+//Utilizar o método foreach para percorrer o array
+//criar a tabela usando o DOM (id do tbody)
--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const openModal = () => {
-    document.getElementById("modal").classList.add("active")
+    document.getElementById('modal').classList.add('active')
 }
 
 const closeModal = () => {
-    document.getElementById("modal").classList.remove("active")
+    document.getElementById('modal').classList.remove('active')
     
 }
 
-document.getElementById("cadastrarUsuario").addEventListener("click", openModal);
+document.getElementById('cadastrarUsuario').addEventListener('click', openModal);
 
 
-document.getElementById("modalClose").addEventListener("click", closeModal);
+document.getElementById('modalClose').addEventListener('click', closeModal);
+
+
+const CreateNewUser = () => {
+
+}
+
